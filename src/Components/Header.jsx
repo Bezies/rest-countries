50c7a7dbb9ea1990384ef4535eb5ea61f6e06a8a
Quentin Bessiere <beziesdev@gmail.com>
2023-11-23 11:19:45 +0100

    test

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -5,7 +5,7 @@ export default function Header() {
     <div className="flex justify-between px-10 py-5 bg-oxford-blue-900">
       <p className="font-bold text-white">Where in the world?</p>
       <button className="flex">
-        <span></span>
+        <span>test</span>
         <span className="ml-2 text-white">Dark Mode</span>
       </button>
     </div>
