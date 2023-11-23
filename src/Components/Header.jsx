ed75a272e5a1c834b0e41a1d0bbaca361e209024
Quentin Bessiere <beziesdev@gmail.com>
2023-11-23 10:19:56 +0100

    test bug

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,14 +1,11 @@
 import React from "react";
-import moonDark from "../assets/moon-solid.svg";
 
 export default function Header() {
   return (
     <div className="flex justify-between px-10 py-5 bg-oxford-blue-900">
       <p className="font-bold text-white">Where in the world?</p>
       <button className="flex">
-        <span>
-          <img className="w-5" src={moonDark} alt="" />
-        </span>
+        <span></span>
         <span className="ml-2 text-white">Dark Mode</span>
       </button>
     </div>
