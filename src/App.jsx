81c43d9d2906d0282f6ae3675dd8e10f8f015ad9
Quentin Bessiere <beziesdev@gmail.com>
2023-11-23 10:10:24 +0100

    start project HTML / CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
 import React from "react";
+import Header from "./Components/Header";
+import SearchCountry from "./Components/SearchCountry";
 
 export default function App() {
-  return <div>App</div>;
+  return (
+    <div className="bg-shark-950 h-screen">
+      <Header />
+      <div>
+        <div className="flex items-center justify-center mt-5 w-full">
+          <SearchCountry />
+        </div>
+      </div>
+    </div>
+  );
 }
---
+++ src/Components/SearchCountry.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function SearchCountry() {
+  return (
+    <input
+      className="w-2/3 pl-10 bg-[url('/img/hero-pattern.svg')]"
+      type="text"
+      placeholder="Search for a country..."
+    />
+  );
+}
